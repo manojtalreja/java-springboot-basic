d74ce430fb695a6f7ac47414928baf5cb43547cb
manojtalreja <talreja_manoj24@yahoo.com>
2024-06-08 14:17:00 +1200

    Update DbCollectorJobsConfig.java

--- src/main/java/com/example/demo/DbCollectorJobsConfig.java
+++ src/main/java/com/example/demo/DbCollectorJobsConfig.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
